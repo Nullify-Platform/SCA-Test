084c250e14904e62af6b15af0c507a63134ee57a
Rahul Tripathi <rahultripathi0401@gmail.com>
2023-01-27 10:27:13 +1100

    remove old js files

--- javascript/src/components/api/fetch.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export const data = async () => {
-    const data = await axios.get("http://127.0.0.1:8000/v1/covid/dates?regex=....-..-..")
-}
--- javascript/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
-import Globe from "./components/globe/Globe";
-
-ReactDOM.render(
-  <Globe/>,
-  document.getElementById("root")
-);
